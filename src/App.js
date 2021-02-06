1e4eb05f9cd418811c56582113b1c9838e338daa
Yulia <sivonenkoyulia@gmail.com>
2021-02-06 19:53:42 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { lazy, Suspense } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import AppBar from './components/AppBar/AppBar';
+import Container from './components/Container/Container';
+import Loader from './components/Loader/Loader';
 
+const HomePage = lazy(() =>
+  import('./views/HomePage/HomePage' /* webpackChunkName: "home-view" */),
+);
+
+const MoviesPage = lazy(() =>
+  import('./views/MoviesPage/MoviesPage' /* webpackChunkName: "home-view" */),
+);
+
+const MovieDetailsPage = lazy(() =>
+  import(
+    './views/MovieDetailsPage/MovieDetailsPage' /* webpackChunkName: "movies-details-view" */
+  ),
+);
+
+function App() {
+  return (
+    <Container>
+      <AppBar />
+
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+
+          <Route path="/movies" exact>
+            <MoviesPage />
+          </Route>
+
+          <Route path="/movies/:movieId">
+            <MovieDetailsPage />
+          </Route>
+
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
+
+      <ToastContainer autoClose={3700} position="bottom-center" />
+    </Container>
+  );
+}
 export default App;
